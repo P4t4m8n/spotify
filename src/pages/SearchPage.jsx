951375b976838d7f38126674714945c9b03a4829
Shoshi <sho3066@gmail.com>
2024-01-05 23:36:39 +0200

    shoshi commit

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -4,15 +4,15 @@ import { SearchResults} from '../cmps/SearchResults'
 
 export function SearchPage() {
 
-const topics=[New,Music,Pop,Hip-Hop,R&B,latino, indi,Rock,Podcusts, Live,Sport,Maditation]
+const ganers=[New,Music,Pop,Hip-Hop,R&B,latino, indi,Rock,Podcusts, Live,Sport,Maditation]
 
 
     return(
         <section className='search-page'>
             <h1>Hi</h1>
         <SearchResults/>
-        {topics.map(topic => {
-        <p>{topic}</p>
+        {ganers.map(ganere => {
+        <p>{ganere}</p>
         })}
                 </section>
 
