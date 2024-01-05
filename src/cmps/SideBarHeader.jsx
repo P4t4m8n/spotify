a2ea5ffb6e49f84f1460801aaddf33006865dd3a
Shoshi <sho3066@gmail.com>
2024-01-05 19:41:54 +0200

    nnn

--- src/cmps/SideBarHeader.jsx
+++ src/cmps/SideBarHeader.jsx
@@ -7,7 +7,7 @@ export function SideBarHeader() {
             <ul className="clean-list">
                 <li key="home"><a href="/" className="flex"><p>🏠</p><span>Home</span></a></li>
                 <li key="search"><a href="/search" className="flex"><p>🔍</p><span>Search</span></a></li>
-            </ul>
+    </ul>
         </div>
     )
 }
