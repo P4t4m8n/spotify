9d987dd934573096e5cf42847c60ebef3abda011
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-06 16:53:34 +0200

    Add RightSidebar and toggle function

---
+++ src/cmps/RightSidebar.jsx
@@ -0,0 +1,13 @@
+
+
+export function RightSidebar() {
+
+    return (
+        <div className="right-sidebar">
+            <div className="flex">
+                <h3>Station(playlist) Title</h3>
+                <button>❎</button>
+            </div>
+        </div>
+    )
+}
