70ffba55d4bfcb92e787c00830b12beb74624ff3
Shoshi <sho3066@gmail.com>
2024-01-06 19:33:41 +0200

    new style

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,22 +1,25 @@
+import { utilService} from '../services/util.service'
 
 import { SearchResults} from '../cmps/SearchResults'
 
 
 export function SearchPage() {
 
-/*const ganers=[New,Music,Pop,Hip-Hop,R&B,latino, indi,Rock,Podcusts, Live,Sport,Maditation]
+const ganers=["New",'Music','Pop','Hip-Hop','R&B','Latino', 'indi','Rock','Podcusts', 'Live','Sport','Maditation','Party music','Electronic music','For sleep']
 
 
     return(
         <section className='search-page'>
-            <h1>Hi</h1>
+            <h1>Brouse all</h1>
         <SearchResults/>
-        {ganers.map(ganere => {
-        <p>{ganere}</p>
-        })}
+        <ul className="ganeres-list">
+        {ganers.map(ganere => 
+        <li key={ganere} style={{backgroundColor: utilService.getRandomColor()}}>{ganere}</li>
+        )}
+        </ul>
                 </section>
 
-    )*/
+    )
 
 
 }
