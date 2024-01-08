6f75171639c0a7ccf732c09aac8d8975f1bef5b3
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-08 02:16:03 +0200

    Almost done LeftSidebarHeader styling psuedo class style missing

--- src/cmps/LeftSidebar/LeftSidebarHeader.jsx
+++ src/cmps/LeftSidebar/LeftSidebarHeader.jsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 
-
 export function SideBarHeader() {
 
     return (
         <div className="left-sidebar-header">
+
             <ul className="clean-list">
-                <li key="home"> <Link to={"/"}><img src="src\assets\img\homefull.svg align-center"></img><span>Home</span></Link></li>
-                <li key="search"><Link to={"/search"}><img src="src\assets\img\search.svg align-center"></img><span>Search</span></Link></li>
-                    </ul>
+
+                <li key="home">
+                    <Link to={"/"} className="flex align-center">
+                        <img src="src\assets\img\homefull.svg" className="left-sidebar-home-icon"></img>
+                        <span>Home</span>
+                    </Link>
+                </li>
+
+                <li key="search">
+                    <Link to={"/search"} className="flex align-center">
+                        <img src="src\assets\img\search.svg" className="left-sidebar-search-icon"></img>
+                        <span>Search</span>
+                    </Link>
+                </li>
+
+            </ul>
+
         </div>
     )
 }
