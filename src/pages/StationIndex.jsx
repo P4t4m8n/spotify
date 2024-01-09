c6b73553741f82439cb6e39ef60f58d29b98c77d
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-09 02:35:04 +0200

    Hero table changes

--- src/pages/StationIndex.jsx
+++ src/pages/StationIndex.jsx
@@ -35,30 +35,87 @@ export function StationIndex() {
     if (!stations) return <div>...Loading</div>
 
     return (
-        <Fragment>
+        <Fragment >
+            <div className="home-page">
+                <ul className="main-hero" >
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 1</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 2</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 3</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 4</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 5</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
+                    <li>
+                        <div className="content">
+                            <img src="src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                            <p>Daily Mix 6</p>
+                        </div>
+                        <button className="hero-play-button">
+                            <img className="icon-16" src="src/assets/img/play.svg" alt="Play" />
+                        </button>
+                    </li>
 
-            <ul className="main-hero" >
+                </ul>
+                {/* <ul className="main-hero" >
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 1</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 2</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 3</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 4</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 5</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
                 <li><img src="src\assets\img\asset 53.jpeg"></img><p>Daily Mix 6</p><button><img className="icon-16" src={`src/assets/img/play.svg`}></img></button></li>
-            </ul>
+            </ul> */}
 
-            {
-                topics.current.map((subHeading, idx) => {
-                    {
-                        const stationsFilterd = stations.filter(station => station.subHeading === subHeading)
-                        return <StationList isPlaying={isPlaying} currStationId={currStation._id
-                        } key={idx} idx={idx}
-                        stations={stationsFilterd} topic={subHeading} onPlayStation={onPlayStation} ></StationList>
-                    }
-                })
-            }
-       
+                {
+                    topics.current.map((subHeading, idx) => {
+                        {
+                            const stationsFilterd = stations.filter(station => station.subHeading === subHeading)
+                            return <StationList isPlaying={isPlaying} currStationId={currStation._id
+                            } key={idx} idx={idx}
+                                stations={stationsFilterd} topic={subHeading} onPlayStation={onPlayStation} ></StationList>
+                        }
+                    })
+                }
 
-            </Fragment>
+            </div>
+        </Fragment>
     )
 
 }
