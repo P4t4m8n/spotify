4259e1c1593e39d5ea0f7ef75ed41a0dbd264caf
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-12 23:53:41 +0200

    Minor fix

--- src/cmps/main/Playlist.jsx
+++ src/cmps/main/Playlist.jsx
@@ -4,38 +4,38 @@ import { LikeCard } from "./LikeCard";
 export function Playlist({ songs, onRemoveSong, isEdit }) {
 
     return (
-        <table className="song-list">
-            <thead className="list-header">
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                    <th><img className="icon-16" style={{ width: '2rem', height: '2rem' }} src="/src/assets/img/clock.svg"></img></th>
-                </tr>
-            </thead>
-            <tbody>
+
+        <div>
+            <ul className="song-list grid clean-list">
+                <li className="list-header">
+                    <p>#</p>
+                    <p>Title</p>
+                    <p>Artist</p>
+                    <p>Album</p>
+                    <p><img className="icon-16" style={{ width: '2rem', height: '2rem' }} src="/src/assets/img/clock.svg"></img></p>
+                </li>
 
                 {
                     songs.map((song, idx) =>
-                        <tr key={song._id} className="station-details-list">
-                            <td>{idx + 1}</td>
-                            <td>{song.title}</td>
-                            <td>{song.artist}</td>
-                            <td>{song.album}</td>
-                            <td>
+                        <li key={song._id} className="station-details-list">
+                            <p>{idx + 1}</p>
+                            <p>{song.title}</p>
+                            <p>{song.artist}</p>
+                            <p>{song.album}</p>
+                            <p>
                                 <div className="details-list-control">
                                     <LikeCard item={song}></LikeCard>
                                     <p>{song.duration}</p>
                                     <button><img className="icon-16" style={{ width: '2rem', height: '2rem' }} src="/src/assets/img/dotsSmall.svg"></img></button>
                                     {isEdit && <button onClick={(ev) => onRemoveSong(ev, song._id)}>❌</button>}
                                 </div>
-                            </td>
-                        </tr>
+                            </p>
+                        </li>
                     )
                 }
-            </tbody>
-        </table>
+
+            </ul>
+        </div>
     )
 
 
