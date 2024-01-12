41cec972d6f75326d36c96aa7c508c758bbaf433
P4t4m8n <you@example.com>
2024-01-12 19:22:09 +0200

    Add color chng header

--- src/pages/StationDetails.jsx
+++ src/pages/StationDetails.jsx
@@ -1,51 +1,69 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router"
 import { loadStation } from "../store/actions/station.actions"
 import { useSelector } from "react-redux"
 import { Playlist } from "../cmps/main/Playlist"
 import { LikeCard } from "../cmps/main/LikeCard"
 import { PlayCard } from "../cmps/main/PlayCard"
+import ColorThief from 'colorthief'
+
 
 
 export function StationDetails() {
 
-    const isPlaying = useSelector(storeState => storeState.songMoudle.isPlaying)
-
     const [currStation, setCurrStation] = useState(null)
+    const [gradient, setGradient] = useState(null)
 
     const params = useParams()
 
     useEffect(() => {
         onLoadstation()
-
     }, [params.stationId])
+
+    useEffect(() => {
+        if (currStation) {
+            const img = new Image()
+            img.crossOrigin = 'Anonymous'
+            img.src = currStation.imgUrl
+
+            img.onload = async () => {
+                const colorThief = new ColorThief()
+                const palette = colorThief.getPalette(img, 5)
+                const gradientColors = palette.map(rgb => `rgb(${rgb.join(',')})`)
+                setGradient(`linear-gradient(to right, ${gradientColors.join(', ')})`)
+            }
+        }
+
+    }, [currStation])
+
 
     async function onLoadstation() {
         const station = await loadStation(params.stationId)
 
         setCurrStation(prevStation => ({ ...prevStation, ...station }))
+
     }
 
     if (!currStation) return <div>...Loading</div>
 
-    const { stationImgUrl, type, createdBy, name, duration, songs, description } = currStation
+    const { imgUrl, type, createdBy, name, duration, songs, description } = currStation
 
     const amount = currStation.songs.length
 
     // console.log('Render station-details')
 
     return (
-        <section className="station-details">
-            <header>
-                <img src="\src\assets\img\asset 53.jpeg"></img>
+        <section className="station-details" >
+            <header className="station-header" style={{ background: gradient }}>
+                <img src={imgUrl}></img>
                 <div className="station-header-info">
-                    <h2>{type === 'station' ? 'Playlist' : 'Episode'}</h2>
+                    <h2>{type === 'station' ? 'Playlist' : 'Song'}</h2>
                     <h3>{name}</h3>
                     <h4>{description}</h4>
-                    <div className="station-header-file-details">
+                    <div >
                         <img src="\src\assets\img\spotify.svg"></img>
                         <p>{createdBy.username || 'Spotify'}</p>
-                        <p>{amount}</p>
+                        <p>{amount} songs</p>
                         <p>about {duration}</p>
                     </div>
                 </div>
@@ -73,31 +91,3 @@ export function StationDetails() {
 
 
 
-
-
-
-
-{/* {stationMoudle &&
-                        <ul>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> Add to Your Library</button></li>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> Remove from profile</button></li>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> Edit details</button></li>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> delete</button></li>
-                            <li><button> Exclude from your taste profile</button></li>
-                            <li><p><img src="\src\assets\img\addToLib.svg"></img> Add to folder</p>
-                                <ul>
-                                    <li><button>Find a folder</button></li>
-                                    <li><button>Create folder</button></li>
-                                    <li><button>Remove from folders</button></li>
-                                </ul>
-                            </li>
-                            <li><p><img src="\src\assets\img\addToLib.svg"></img>Share</p>
-                                <ul>
-                                    <li><button> <img src="\src\assets\img\addToLib.svg"></img>Copy link to playlist</button></li>
-                                    <li><button><img src="\src\assets\img\addToLib.svg"></img>Embed playlist</button></li>
-                                </ul>
-                            </li>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> delete</button></li>
-                            <li><button><img src="\src\assets\img\addToLib.svg"></img> delete</button></li>
-                        </ul>
-                    } */}
