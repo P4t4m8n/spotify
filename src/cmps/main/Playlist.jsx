c3ffe6b113461cbb5e9d2c6d1aa5983122344d86
P4t4m8n <you@example.com>
2024-01-14 00:48:04 +0200

    avcc

--- src/cmps/main/Playlist.jsx
+++ src/cmps/main/Playlist.jsx
@@ -1,7 +1,7 @@
 import { Clock, Dots } from "../../services/icons.service";
 import { LikeCard } from "./LikeCard";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-
+import { DndContext } from '@dnd-kit/core';
+import { Draggable } from './Draggable';
 
 
 export function Playlist({ songs, onRemoveSong, isEdit }) {
@@ -21,27 +21,31 @@ export function Playlist({ songs, onRemoveSong, isEdit }) {
 
             </ul>
             <div className="mother-fucking-line"></div>
-            <ul className="song-list grid clean-list">
-                {songs.map((song, idx) => (
-                    <li key={song._id} className="station-details-list">
-                        <p>{idx + 1}</p>
-                        <div className="artist-and-image grid">
-                            <img src={song.imgUrl} alt={song.title} /> {song.title}
-                        </div>
-                        <p>{song.artist}</p>
-                        <p>{song.album}</p>
-                        <div>
-                            <div className="details-list-control">
-                                <LikeCard item={song}></LikeCard>
-                                <p>{song.duration}</p>
-                                <button><Dots></Dots></button>
-                                {isEdit && <button onClick={(ev) => onRemoveSong(ev, song._id)}>❌</button>}
-                            </div>
-                        </div>
-                    </li>
-                ))}
+            <DndContext >
+                <ul className="song-list grid clean-list">
+                    {songs.map((song, idx) => (
+                        <Draggable key={song._id}>
+                            <li key={song._id} className="station-details-list">
+                                <p>{idx + 1}</p>
+                                <div className="artist-and-image grid">
+                                    <img src={song.imgUrl} alt={song.title} /> {song.title}
+                                </div>
+                                <p>{song.artist}</p>
+                                <p>{song.album}</p>
+                                <div>
+                                    <div className="details-list-control">
+                                        <LikeCard item={song}></LikeCard>
+                                        <p>{song.duration}</p>
+                                        <button><Dots></Dots></button>
+                                        {isEdit && <button onClick={(ev) => onRemoveSong(ev, song._id)}>❌</button>}
+                                    </div>
+                                </div>
+                            </li>
+                        </Draggable>
+                    ))}
 
-            </ul>
+                </ul>
+            </DndContext>
         </div >
     )
 }
