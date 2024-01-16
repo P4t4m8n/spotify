3a47b1bdcde00c36d7d825ed8f3e5bee0e7d6056
P4t4m8n <eranopenuni@gmail.com>
2024-01-17 00:18:34 +0200

    aaa

--- src/pages/StationIndex.jsx
+++ src/pages/StationIndex.jsx
@@ -4,11 +4,13 @@ import { StationList } from "../cmps/main/StationList"
 import { stationService } from "../services/station.service"
 import { loadStations } from "../store/actions/station.actions"
 import { useBackgroundFromImage } from "../cmps/CustomHooks/useBackgroundFromImage"
-import { useDeviceCheck } from "../cmps/CustomHooks/UseDeviceCheck"
+import { PC, useDeviceCheck } from "../cmps/CustomHooks/UseDeviceCheck"
 
 export function StationIndex() {
 
     const stations = useSelector(storeState => storeState.stationsMoudle.stations)
+    const device = useSelector(storeState => storeState.appMoudle.device)
+
 
     const stationListTitle = useRef([])
 
@@ -22,74 +24,72 @@ export function StationIndex() {
 
 
     if (!stations) return <div>...Loading</div>
-    // console.log('Render stationIndex')
-
-
-   
 
     return (
         <Fragment >
             <div className="home-page">
-                <h1>Good evening</h1>
-                <ul className="main-hero" >
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 1</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 2</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 3</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 4</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 5</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
-                    <li>
-                        <div className="content">
-                            <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
-                            <p>Daily Mix 6</p>
-                        </div>
-                        <button className="hero-play-button">
-                            <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
-                        </button>
-                    </li>
+                {(device === PC) &&
+                    <>
+                        <h1>Good evening</h1>
+                        <ul className="main-hero" >
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 1</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 2</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 3</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 4</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 5</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
+                            <li>
+                                <div className="content">
+                                    <img src="/src/assets/img/asset 53.jpeg" alt="Daily Mix" />
+                                    <p>Daily Mix 6</p>
+                                </div>
+                                <button className="hero-play-button">
+                                    <img className="icon-16" src="/src/assets/img/play.svg" alt="Play" />
+                                </button>
+                            </li>
 
-                </ul>
-
-
+                        </ul>
+                    </>
+                }
                 {
                     stationListTitle.current.map((stationListTitle, idx) => {
                         {
